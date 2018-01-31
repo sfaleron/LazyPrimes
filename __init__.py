06502140d6ffc96724dd2372ce99efb838a20fc6
Chris Fuller <cfuller800@thinkingplanet.net>
2018-01-31 14:01:07 -0600

    Class name got improved, and comments shifted to correct file.

--- __init__.py
+++ __init__.py
@@ -1,9 +1,16 @@
 
+# Adapted from
+# https://gist.github.com/sfaleron/8cabe55fa02e769661a6120be053f87a,
+# which is a port to Python 3.x of the code in
+# http://logn.org/2009/07/lazy-primes-sieve-in-python.html, which is a
+# port to Python 2.x of the Haskell code in
+# https://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf!
+
 import itertools
 
 from . _inner import primes
 
-class SieveGen(object):
+class LazyPrimes(object):
     def __init__(self, k=5, skipto=None):
         self._it = primes(k)
 
--- _inner.py
+++ _inner.py
@@ -1,11 +1,4 @@
 
-# Adapted from
-# https://gist.github.com/sfaleron/8cabe55fa02e769661a6120be053f87a,
-# which is a port to Python 3.x of the code in
-# http://logn.org/2009/07/lazy-primes-sieve-in-python.html, which is a
-# port to Python 2.x of the Haskell code in
-# https://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf!
-
 import heapq
 import operator
 import itertools
